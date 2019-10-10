00f1896994f29783492705bb802c9d786791c8b7
Edigley Fraga <edigley@gmail.com>
2019-10-10 18:08:59 +0200

    Create plot_jonquera_single_core_8_workers.R

---
+++ results/plot_jonquera_single_core_8_workers.R
@@ -0,0 +1,10 @@
+source("https://raw.githubusercontent.com/edigley/spif/master/results/graph_tsp_overview.R")
+
+results <- read.table('https://raw.githubusercontent.com/edigley/cloudsim/master/input-files/jonquera_single_core_8_workers_trace.txt', header = TRUE)
+ws <- subset(results, !is.na(start), select=c( "individual", "start", "end" ))
+
+names(ws) <- c("Worker", "start", "end")
+ws$start  <- ws$start * 10
+ws$end    <- ws$end   * 10
+
+graph_tsp_overview(ws)
