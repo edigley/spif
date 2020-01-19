cacb85598944104fec49e27dcd6eb9b0e2578268
Edigley Fraga <edigley@gmail.com>
2020-01-19 17:09:32 +0100

    Update plot_line_plot_grouped_by_scenario_average_max_cummulated_fitness_per_generation_nsd.R

--- results/plot_line_plot_grouped_by_scenario_average_max_cummulated_fitness_per_generation_nsd.R
+++ results/plot_line_plot_grouped_by_scenario_average_max_cummulated_fitness_per_generation_nsd.R
@@ -1,12 +1,12 @@
 source("https://raw.githubusercontent.com/edigley/spif/master/results/function_generate_aggregated_stats.R")
 
-resultsTSPAgof <- read.table('https://raw.githubusercontent.com/edigley/spif/master/results/two_stage_prediction_agof.txt', header=T, row.names=NULL)
+resultsTSPNSD <- read.table('https://raw.githubusercontent.com/edigley/spif/master/results/two_stage_prediction.txt', header=T, row.names=NULL)
 
-dsAll <- generateAggregatedStats(resultsTSPAgof)
+dsAll <- generateAggregatedStats(resultsTSPNSD)
 
 #summary(dsAll)
 #head(dsAll)
 
-ggplot(data=dsAll, aes(x=generation, y=maxCummFitnees, group=scenario)) +
+ggplot(data=dsAll, aes(x=generation, y=minCummFitnees, group=scenario)) +
   geom_line(aes(color=scenario)) +
 geom_point(aes(color=scenario))
