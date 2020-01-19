1da0afd513ac0f1bac11ea0cf3fa02fc986cab95
Edigley Fraga <edigley@gmail.com>
2020-01-19 17:04:43 +0100

    Update plot_line_plot_grouped_by_scenario_average_max_cummulated_fitness_per_generation_agof.R

--- results/plot_line_plot_grouped_by_scenario_average_max_cummulated_fitness_per_generation_agof.R
+++ results/plot_line_plot_grouped_by_scenario_average_max_cummulated_fitness_per_generation_agof.R
@@ -1,6 +1,8 @@
 source("https://raw.githubusercontent.com/edigley/spif/master/results/function_generate_aggregated_stats.R")
+
 resultsTSPAgof <- read.table('https://raw.githubusercontent.com/edigley/spif/master/results/two_stage_prediction_agof.txt', header=T, row.names=NULL)
-dsAll <- generateAggregatedStats(resultsTSPAll)
+
+dsAll <- generateAggregatedStats(resultsTSPAgof)
 
 #summary(dsAll)
 #head(dsAll)
